37c95a246ced1b46d4b867c0a512d444e3f83ffd
clairelyles <claire.lyles@gmail.com>
2015-03-18 11:11:06 -0700

    added route params and played around with markers/mapbox

--- assets/js/controllers/HomeCtrl.js
+++ assets/js/controllers/HomeCtrl.js
@@ -1,13 +1,40 @@
 studioApp.controller('HomeCtrl', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
 
-  angular.extend($scope, {
-    defaults: {
-      scrollWheelZoom: false
-    },
-    center: {
-        lat: 47.623354,
-        lng: -122.330112,
-        zoom: 12
+  // angular.extend($scope, {
+  //   defaults: {
+  //     scrollWheelZoom: false
+  //   },
+  //   center: {
+  //       lat: 47.623354,
+  //       lng: -122.330112,
+  //       zoom: 12
+  //   }
+  // })
+
+
+  function _init(){
+    $scope.center = {
+      lat: 47.623354,
+      lng: -122.330112,
+      zoom: 12
+    };
+    //randomly place markers on the map
+    var westLng = 34.770956;
+    var eastLng = 34.832754;
+    var northLat = 32.098787;
+    var southLat = 32.05261;
+    var numberOfMarkers = 100;
+    $scope.markers = {};
+    for(var i = 0; i<numberOfMarkers; i++){
+      var randomLng = (Math.random() * (eastLng - westLng) + westLng).toFixed(6);
+      var randomLat = (Math.random() * (northLat - southLat) + southLat).toFixed(6);
+      $scope.markers['marker_' + i] = {
+        lat: parseFloat(randomLat),
+        lng: parseFloat(randomLng),
+      };
     }
-  })
+  }
+
+   _init();
+
 }])
--- assets/js/controllers/ShowCtrl.js
+++ assets/js/controllers/ShowCtrl.js
@@ -1,4 +1,7 @@
 studioApp.controller('ShowCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
 
+  $scope.studio = {
+    id: $routeParams.id
+  }
 
 }])
